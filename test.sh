cabe4775e561bbd3d177726541ccf85a91fa6073
Fulll3 <kovarik.adam@email.cz>
2024-11-13 20:33:49 +0100

    wip

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Call the script located at /root/test.sh
-/root/test.sh
+
+/opt/ai-dock/bin/init.sh
+
+wget -O - "https://raw.githubusercontent.com/zenpalio/vastai-pyworker/main/setup_automatic.sh" | bash && (text-generation-launcher --model-id "$MODEL_ID" --json-output --port 5001 --hostname "0.0.0.0" &>> $MODEL_LOG);
