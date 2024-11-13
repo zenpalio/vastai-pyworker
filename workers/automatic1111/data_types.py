f8913722014d817b301e50ef69866ff69753260c
Fulll3 <kovarik.adam@email.cz>
2024-11-13 17:55:49 +0100

    wip

--- workers/automatic1111/data_types.py
+++ workers/automatic1111/data_types.py
@@ -13,36 +13,36 @@ from typing import List, Optional, Union, Dict, Any
 
 @dataclass
 class ADetailerArgs:
-    ad_cfg_scale: Optional[int] = 7
-    ad_checkpoint: Optional[str] = "Use same checkpoint"
-    ad_clip_skip: Optional[int] = 1
-    ad_confidence: Optional[float] = 0.3
-    ad_controlnet_guidance_end: Optional[int] = 1
+    ad_cfg_scale: Optional[int] = 0
+    ad_checkpoint: Optional[str] = ""
+    ad_clip_skip: Optional[int] = 0
+    ad_confidence: Optional[float] = 0.0
+    ad_controlnet_guidance_end: Optional[int] = 0
     ad_controlnet_guidance_start: Optional[int] = 0
-    ad_controlnet_model: Optional[str] = "None"
-    ad_controlnet_module: Optional[str] = "None"
-    ad_controlnet_weight: Optional[int] = 1
-    ad_denoising_strength: Optional[float] = 0.4
-    ad_dilate_erode: Optional[int] = 4
-    ad_inpaint_height: Optional[int] = 512
-    ad_inpaint_only_masked: Optional[bool] = True
-    ad_inpaint_only_masked_padding: Optional[int] = 32
-    ad_inpaint_width: Optional[int] = 512
-    ad_mask_blur: Optional[int] = 4
+    ad_controlnet_model: Optional[str] = ""
+    ad_controlnet_module: Optional[str] = ""
+    ad_controlnet_weight: Optional[int] = 0
+    ad_denoising_strength: Optional[float] = 0.0
+    ad_dilate_erode: Optional[int] = 0
+    ad_inpaint_height: Optional[int] = 0
+    ad_inpaint_only_masked: Optional[bool] = False
+    ad_inpaint_only_masked_padding: Optional[int] = 0
+    ad_inpaint_width: Optional[int] = 0
+    ad_mask_blur: Optional[int] = 0
     ad_mask_k_largest: Optional[int] = 0
-    ad_mask_max_ratio: Optional[int] = 1
-    ad_mask_merge_invert: Optional[str] = "None"
+    ad_mask_max_ratio: Optional[int] = 0
+    ad_mask_merge_invert: Optional[str] = ""
     ad_mask_min_ratio: Optional[int] = 0
-    ad_model: Optional[str] = "face_yolov8n.pt"
+    ad_model: Optional[str] = ""
     ad_model_classes: Optional[str] = ""
     ad_negative_prompt: Optional[str] = ""
-    ad_noise_multiplier: Optional[int] = 1
+    ad_noise_multiplier: Optional[int] = 0
     ad_prompt: Optional[str] = ""
     ad_restore_face: Optional[bool] = False
-    ad_sampler: Optional[str] = "DPM++ 2M"
-    ad_scheduler: Optional[str] = "Use same scheduler"
-    ad_steps: Optional[int] = 28
-    ad_tab_enable: Optional[bool] = True
+    ad_sampler: Optional[str] = ""
+    ad_scheduler: Optional[str] = ""
+    ad_steps: Optional[int] = 0
+    ad_tab_enable: Optional[bool] = False
     ad_use_cfg_scale: Optional[bool] = False
     ad_use_checkpoint: Optional[bool] = False
     ad_use_clip_skip: Optional[bool] = False
@@ -51,7 +51,7 @@ class ADetailerArgs:
     ad_use_sampler: Optional[bool] = False
     ad_use_steps: Optional[bool] = False
     ad_use_vae: Optional[bool] = False
-    ad_vae: Optional[str] = "Use same VAE"
+    ad_vae: Optional[str] = ""
     ad_x_offset: Optional[int] = 0
     ad_y_offset: Optional[int] = 0
     is_api: Optional[List[Any]] = dataclasses.field(default_factory=list)
@@ -67,38 +67,38 @@ class ADetailer:
 @dataclass
 class ControlNetArgs:
     advanced_weighting: Optional[Any] = None
-    animatediff_batch: Optional[bool] = None
+    animatediff_batch: Optional[bool] = False
     batch_image_files: Optional[List[str]] = dataclasses.field(default_factory=list)
-    batch_images: Optional[str] = None
+    batch_images: Optional[str] = ""
     batch_keyframe_idx: Optional[Any] = None
     batch_mask_dir: Optional[Any] = None
     batch_modifiers: Optional[List[str]] = dataclasses.field(default_factory=list)
-    control_mode: Optional[str] = None
+    control_mode: Optional[str] = ""
     effective_region_mask: Optional[Any] = None
-    enabled: Optional[bool] = None
-    guidance_end: Optional[float] = None
-    guidance_start: Optional[float] = None
-    hr_option: Optional[str] = None
+    enabled: Optional[bool] = False
+    guidance_end: Optional[float] = 0.0
+    guidance_start: Optional[float] = 0.0
+    hr_option: Optional[str] = ""
     image: Optional[Any] = None
-    inpaint_crop_input_image: Optional[bool] = None
-    input_mode: Optional[str] = None
+    inpaint_crop_input_image: Optional[bool] = False
+    input_mode: Optional[str] = ""
     ipadapter_input: Optional[Any] = None
-    is_ui: Optional[bool] = None
-    loopback: Optional[bool] = None
-    low_vram: Optional[bool] = None
+    is_ui: Optional[bool] = False
+    loopback: Optional[bool] = False
+    low_vram: Optional[bool] = False
     mask: Optional[Any] = None
-    model: Optional[str] = None
-    module: Optional[str] = None
-    output_dir: Optional[str] = None
-    pixel_perfect: Optional[bool] = None
-    processor_res: Optional[int] = None
-    pulid_mode: Optional[str] = None
-    resize_mode: Optional[str] = None
-    save_detected_map: Optional[bool] = None
-    threshold_a: Optional[float] = None
-    threshold_b: Optional[float] = None
-    union_control_type: Optional[str] = None
-    weight: Optional[float] = None
+    model: Optional[str] = ""
+    module: Optional[str] = ""
+    output_dir: Optional[str] = ""
+    pixel_perfect: Optional[bool] = False
+    processor_res: Optional[int] = 0
+    pulid_mode: Optional[str] = ""
+    resize_mode: Optional[str] = ""
+    save_detected_map: Optional[bool] = False
+    threshold_a: Optional[float] = 0.0
+    threshold_b: Optional[float] = 0.0
+    union_control_type: Optional[str] = ""
+    weight: Optional[float] = 0.0
 
 
 @dataclass
@@ -173,48 +173,48 @@ class AlwaysOnScripts:
 @dataclass
 class InputData:
     alwayson_scripts: Optional[AlwaysOnScripts] = None
-    batch_size: Optional[int] = None
-    cfg_scale: Optional[int] = None
+    batch_size: Optional[int] = 0
+    cfg_scale: Optional[int] = 0
     comments: Optional[Dict[str, Any]] = None
-    denoising_strength: Optional[float] = None
-    disable_extra_networks: Optional[bool] = None
-    do_not_save_grid: Optional[bool] = None
-    do_not_save_samples: Optional[bool] = None
-    enable_hr: Optional[bool] = None
-    height: Optional[int] = None
-    hr_negative_prompt: Optional[str] = None
-    hr_prompt: Optional[str] = None
-    hr_resize_x: Optional[int] = None
-    hr_resize_y: Optional[int] = None
-    hr_scale: Optional[int] = None
-    hr_scheduler: Optional[str] = None
-    hr_second_pass_steps: Optional[int] = None
-    hr_upscaler: Optional[str] = None
-    n_iter: Optional[int] = None
-    negative_prompt: Optional[str] = None
+    denoising_strength: Optional[float] = 0.0
+    disable_extra_networks: Optional[bool] = False
+    do_not_save_grid: Optional[bool] = False
+    do_not_save_samples: Optional[bool] = False
+    enable_hr: Optional[bool] = False
+    height: Optional[int] = 0
+    hr_negative_prompt: Optional[str] = ""
+    hr_prompt: Optional[str] = ""
+    hr_resize_x: Optional[int] = 0
+    hr_resize_y: Optional[int] = 0
+    hr_scale: Optional[int] = 0
+    hr_scheduler: Optional[str] = ""
+    hr_second_pass_steps: Optional[int] = 0
+    hr_upscaler: Optional[str] = ""
+    n_iter: Optional[int] = 0
+    negative_prompt: Optional[str] = ""
     override_settings: Optional[Dict[str, int]] = None
-    override_settings_restore_afterwards: Optional[bool] = None
-    prompt: Optional[str] = None
-    restore_faces: Optional[bool] = None
-    s_churn: Optional[float] = None
-    s_min_uncond: Optional[float] = None
-    s_noise: Optional[float] = None
+    override_settings_restore_afterwards: Optional[bool] = False
+    prompt: Optional[str] = ""
+    restore_faces: Optional[bool] = False
+    s_churn: Optional[float] = 0.0
+    s_min_uncond: Optional[float] = 0.0
+    s_noise: Optional[float] = 0.0
     s_tmax: Optional[Any] = None
-    s_tmin: Optional[float] = None
-    sampler_name: Optional[str] = None
-    scheduler: Optional[str] = None
+    s_tmin: Optional[float] = 0.0
+    sampler_name: Optional[str] = ""
+    scheduler: Optional[str] = ""
     script_args: Optional[List[Any]] = dataclasses.field(default_factory=list)
     script_name: Optional[Any] = None
-    seed: Optional[int] = None
-    seed_enable_extras: Optional[bool] = None
-    seed_resize_from_h: Optional[int] = None
-    seed_resize_from_w: Optional[int] = None
-    steps: Optional[int] = None
+    seed: Optional[int] = 0
+    seed_enable_extras: Optional[bool] = False
+    seed_resize_from_h: Optional[int] = 0
+    seed_resize_from_w: Optional[int] = 0
+    steps: Optional[int] = 0
     styles: Optional[List[Any]] = dataclasses.field(default_factory=list)
-    subseed: Optional[int] = None
-    subseed_strength: Optional[float] = None
-    tiling: Optional[bool] = None
-    width: Optional[int] = None
+    subseed: Optional[int] = 0
+    subseed_strength: Optional[float] = 0.0
+    tiling: Optional[bool] = False
+    width: Optional[int] = 0
 
     def generate_payload_json(self) -> Dict[str, Any]:
         return json.dumps(dataclasses.asdict(self)).encode("utf-8")
