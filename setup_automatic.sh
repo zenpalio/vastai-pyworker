beef93a6a98cca1968884d817fb9c73127c2c4f8
Fulll3 <kovarik.adam@email.cz>
2024-11-13 18:14:06 +0100

    adding register endpoint call to automatic setup

--- setup_automatic.sh
+++ setup_automatic.sh
@@ -43,7 +43,26 @@ requests.post(
     json=opt_json,
     verify=False,  # Disable SSL verification
 )
+# Send POST request to REPORT_ADDR
+report_addr = os.environ["REPORT_ADDR"]
+container_id = os.environ["CONTAINER_ID"]
+public_ip = os.environ["PUBLIC_IPADDR"]
+worker_port = os.environ["WORKER_PORT"]
+vast_tcp_port = os.environ[f"VAST_TCP_PORT_{worker_port}"]
+full_url = f"{public_ip}:{vast_tcp_port}"
+
+data = {
+    "id": container_id,
+    "url": full_url
+}
+
+requests.post(
+    url=f"{report_addr}/public/v1/webhook/vastai/automatic/register",
+    json=data,
+    verify=False,  # Disable SSL verification
+)
 EOF
 
 # Call the Python script
 python3 script.py
+
