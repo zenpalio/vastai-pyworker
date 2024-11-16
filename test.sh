ebbd6c310d0cd5497d0fafbce54d0c8319513ccc
Fulll3 <kovarik.adam@email.cz>
2024-11-16 18:53:15 +0100

    wip

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
-/opt/ai-dock/bin/init.sh
+export BACKEND=tgi
+wget -O - "https://raw.githubusercontent.com/zenpalio/vastai-pyworker/main/start_server.sh" | bash && (text-generation-launcher --model-id "$MODEL_ID" --json-output --port 5001 --hostname "0.0.0.0" &>> $MODEL_LOG)
 
-wget -O - "https://raw.githubusercontent.com/zenpalio/vastai-pyworker/main/setup_automatic.sh" | bash && (text-generation-launcher --model-id "$MODEL_ID" --json-output --port 5001 --hostname "0.0.0.0" &>> $MODEL_LOG);
+
+echo "Creating and running the model"
+ollama serve &
+sleep 10
+ollama run llama3-uncensored
