b6af2a09b47beff507d233db9b6e2054bdab61fe
Fulll3 <kovarik.adam@email.cz>
2024-11-16 18:25:05 +0100

    wip

--- setup_ollama.sh
+++ setup_ollama.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# wget https://huggingface.co/Orenguteng/Llama-3.1-8B-Lexi-Uncensored-GGUF/resolve/main/Llama-3.1-8B-Lexi-Uncensored_F16.gguf
+export BACKEND=tgi
+wget -O - "https://raw.githubusercontent.com/zenpalio/vastai-pyworker/main/start_server.sh" | bash && (text-generation-launcher --model-id "$MODEL_ID" --json-output --port 5001 --hostname "0.0.0.0" &>> $MODEL_LOG)
+
+wget https://huggingface.co/Orenguteng/Llama-3.1-8B-Lexi-Uncensored-GGUF/resolve/main/Llama-3.1-8B-Lexi-Uncensored_F16.gguf
 # Define the template content
 echo "Creating the model file2"
 cat << 'EOF' > llama3-uncensored.modelfile
@@ -26,9 +29,9 @@ EOF
 
 # Create and run the model using ollama
 echo "Creating and running the model"
+ollama serve &
+sleep 20
 ollama create llama3-uncensored -f llama3-uncensored.modelfile 
 ollama run llama3-uncensored
 
 # Export BACKEND variable and download/start the server script
-# export BACKEND=tgi
-# wget -O - "https://raw.githubusercontent.com/zenpalio/vastai-pyworker/main/start_server.sh" | bash && (text-generation-launcher --model-id "$MODEL_ID" --json-output --port 5001 --hostname "0.0.0.0" &>> $MODEL_LOG)
