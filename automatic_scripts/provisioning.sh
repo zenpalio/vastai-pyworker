0cee604256817526f550f4303b6379264b31bfc5
Fulll3 <kovarik.adam@email.cz>
2024-11-20 16:51:44 +0100

    wip

--- automatic_scripts/provisioning.sh
+++ automatic_scripts/provisioning.sh
@@ -212,6 +212,7 @@ fi
 
 if [[ -n $civit_auth_token ]]; then
     url="${1}?token=${civit_auth_token}"
+    printf "Downloading civitai %s\n" "$url"
     wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$url"
 elif [[ -n $auth_token ]]; then
     wget --header="Authorization: Bearer $auth_token" -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
