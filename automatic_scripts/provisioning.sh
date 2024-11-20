579fb3b40653689aff47d1f439e81ae55c953223
Fulll3 <kovarik.adam@email.cz>
2024-11-20 16:03:52 +0100

    wip

--- automatic_scripts/provisioning.sh
+++ automatic_scripts/provisioning.sh
@@ -202,24 +202,20 @@ function provisioning_print_end() {
 
 # Download from $1 URL to $2 file path
 function provisioning_download() {
-    if 
-        [[ -n $CIVITAI_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\.)?civitai\.com(/|$|\?) ]]; then
-        printf "Downloading from CivitAI\n"
-        civit_auth_token="$CIVITAI_TOKEN"
-    fi
-    elif [[ -n $HF_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\.)?huggingface\.co(/|$|\?) ]]; then
-        printf "Downloading from Hugging Face\n"
-        auth_token="$HF_TOKEN"
-    if
-    if [[ -n $civit_auth_token ]];then
-        url="${1}?token=${civit_auth_token}"
-        printf "url from civitai: %s\n" "${url}"
-        wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$url"
-    if [[ -n $auth_token ]];then
-        wget --header="Authorization: Bearer $auth_token" -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
-    else
-        wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
-    fi
+if [[ -n $CIVITAI_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\.)?civitai\.com(/|$|\?) ]]; then
+    civit_auth_token="$CIVITAI_TOKEN"
+elif [[ -n $HF_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\.)?huggingface\.co(/|$|\?) ]]; then
+    auth_token="$HF_TOKEN"
+fi
+
+if [[ -n $civit_auth_token ]]; then
+    url="${1}?token=${civit_auth_token}"
+    wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$url"
+elif [[ -n $auth_token ]]; then
+    wget --header="Authorization: Bearer $auth_token" -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+else
+    wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+fi
 }
 
 provisioning_start
