f4f6a84258ecadb75b9565a9b0de550868ea2e41
Fulll3 <kovarik.adam@email.cz>
2024-11-26 15:54:40 +0100

    set upaphrodite

--- setup_aphrodite.sh
+++ setup_aphrodite.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Activate the virtual environment
-source .venv/bin/activate
+
 
 # Install necessary packages
 pip install uv
-pip install aphrodite-engine
-pip install setuptools
-pip install ray
+uv venv
+source .venv/bin/activate
+uv pip install aphrodite-engine
+uv pip install setuptools
+uv pip install ray
 
-# Run aphrodite
+
 aphrodite run Orenguteng/Llama-3-8B-Lexi-Uncensored
