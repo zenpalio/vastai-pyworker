868dc9ab9bae40fe845b5a429c83f406b7632583
Fulll3 <kovarik.adam@email.cz>
2024-12-02 14:40:46 +0100

    wip aphrodite

--- setup_aphrodite.sh
+++ setup_aphrodite.sh
@@ -11,7 +11,7 @@ uv pip install aphrodite-engine
 uv pip install setuptools
 uv pip install ray
 
-wget -O - "https://raw.githubusercontent.com/zenpalio/vastai-pyworker/main/start_server.sh" | bash && (text-generation-launcher --model-id "$MODEL_ID" --json-output --port 5001 --hostname "0.0.0.0" &>> $MODEL_LOG); &
+wget -O - "https://raw.githubusercontent.com/zenpalio/vastai-pyworker/main/start_server.sh" | bash && (text-generation-launcher --model-id "$MODEL_ID" --json-output --port 5001 --hostname "0.0.0.0" &>> $MODEL_LOG) &
 
 aphrodite run Orenguteng/Llama-3-8B-Lexi-Uncensored
 
