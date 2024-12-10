c79fc94affcd9f2368d3a5c14008b8dc83e0cdd2
Fulll3 <kovarik.adam@email.cz>
2024-12-10 14:56:55 +0100

    wip

--- setup_aphrodite_runpod.sh
+++ setup_aphrodite_runpod.sh
@@ -15,10 +15,10 @@ uv pip install aphrodite-engine
 uv pip install setuptools
 uv pip install ray
 
-echo "starting aphrodite"
-aphrodite run Orenguteng/Llama-3.1-8B-Lexi-Uncensored-V2 -tp 2 &
+#echo "starting aphrodite"
+#aphrodite run Orenguteng/Llama-3.1-8B-Lexi-Uncensored-V2 -tp 2 &
 
-sleep 60
+#sleep 60
 echo "starting server"
 wget -O - "https://raw.githubusercontent.com/zenpalio/vastai-pyworker/main/start_server.sh" | bash && (text-generation-launcher --model-id "$MODEL_ID" --json-output --port 5001 --hostname "0.0.0.0" &>> $MODEL_LOG)
 
