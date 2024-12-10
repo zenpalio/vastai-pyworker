d1a1cc0ed069b512da1b46bbce1e38e30638a50d
Fulll3 <kovarik.adam@email.cz>
2024-12-10 15:17:59 +0100

    runpod

--- setup_aphrodite_runpod.sh
+++ setup_aphrodite_runpod.sh
@@ -12,11 +12,11 @@ apt install curl -y;
 pip install uv
 uv venv
 source .venv/bin/activate
-#uv pip install aphrodite-engine
-#uv pip install setuptools
-#uv pip install ray
-#echo "starting aphrodite"
-#aphrodite run Orenguteng/Llama-3.1-8B-Lexi-Uncensored-V2 -tp 2 &
+uv pip install aphrodite-engine
+uv pip install setuptools
+uv pip install ray
+echo "starting aphrodite"
+aphrodite run Orenguteng/Llama-3.1-8B-Lexi-Uncensored-V2 -tp 2 &
 
 #sleep 60
 echo "starting server"
