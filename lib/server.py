0d8a2c81564e93dc9044125fa159379a5d85628e
Fulll3 <kovarik.adam@email.cz>
2024-12-18 14:23:17 +0100

    removing register call

--- lib/server.py
+++ lib/server.py
@@ -1,39 +1,16 @@
-import json
 import os
 import logging
 from typing import List
 import ssl
 from asyncio import run, gather
-from urllib.parse import urljoin
 
-import requests
 
 
 from lib.backend import Backend
 from aiohttp import web
 
-from lib.metrics import get_container_id, get_provider, get_url
 
 log = logging.getLogger(__file__)
-
-
-def register_public_ip():
-    report_addr = os.environ["REPORT_ADDR"]
-    for addr in report_addr.split(","):
-        full_path = urljoin(addr, "/public/v1/webhook/instance/register/")
-        data = {
-            "url": get_url(),
-            "id": get_container_id(),
-            "type": os.environ["BACKEND"],
-            "provider": get_provider(),
-        }
-        log.debug("register payload")
-        print(data)
-        try:
-            requests.post(full_path, json=data, timeout=1)
-        except Exception as e:
-            log.debug(f"autoscaler status update failed with error: {e}")
-
 
 def start_server(backend: Backend, routes: List[web.RouteDef], **kwargs):
     log.debug("getting certificate...")
@@ -59,7 +36,6 @@ def start_server(backend: Backend, routes: List[web.RouteDef], **kwargs):
             port=int(os.environ["WORKER_PORT"]),
             **kwargs,
         )
-        register_public_ip()
         await gather(site.start(), backend._start_tracking())
 
     run(main())
