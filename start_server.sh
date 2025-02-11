d3a25ac6b54a0c4709aa6b7337a672d8ba6bde7f
Fulll3 <kovarik.adam@email.cz>
2025-02-11 17:07:56 +0100

    fixing depencies

--- start_server.sh
+++ start_server.sh
@@ -9,7 +9,7 @@ ENV_PATH="$WORKSPACE_DIR/worker-env"
 DEBUG_LOG="$WORKSPACE_DIR/debug.log"
 PYWORKER_LOG="$WORKSPACE_DIR/pyworker.log"
 
-REPORT_ADDR="${REPORT_ADDR:-https://api.dev.mybabes.ai,https://api.qa.mybabes.ai,https://api.mybabes.ai}"
+REPORT_ADDR="${REPORT_ADDR:-https://api.mybabes.ai}"
 USE_SSL="${USE_SSL:-true}"
 WORKER_PORT="${WORKER_PORT:-3000}"
 BACKEND="${BACKEND:-automatic1111}"
