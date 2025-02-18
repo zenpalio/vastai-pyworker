97c04f0041ab46cba869abf50b0235500eb640a6
Fulll3 <kovarik.adam@email.cz>
2025-02-18 17:02:49 +0100

    wip

--- setup_automatic.sh
+++ setup_automatic.sh
@@ -10,7 +10,8 @@ function setup_gsutils() {
 
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
     echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-    sudo apt-get install google-cloud-cli -y
+    export DEBIAN_FRONTEND=noninteractive
+    DEBIAN_FRONTEND=noninteractive sudo apt-get install google-cloud-cli -y
 
     echo "$SERVICE_ACCOUNT_JSON" > ./zenpalio-f0ec2f137303.json
 
