39942fe8e9d72d10a0e852a27689fcdc8a4923cf
Fulll3 <kovarik.adam@email.cz>
2025-02-18 15:48:04 +0100

    wip

--- automatic_scripts/provisioning.sh
+++ automatic_scripts/provisioning.sh
@@ -100,7 +100,6 @@ function provisioning_start() {
     # We need to apply some workarounds to make old builds work with the new default
     printf "Starting provisioning...\n"
     printf "custom provisioning\n"
-    setup_gsutils
     if [[ ! -d /opt/environments/python ]]; then 
         export MAMBA_BASE=true
     fi
@@ -146,21 +145,6 @@ function provisioning_start() {
             ${ARGS_COMBINED}
     fi
     provisioning_print_end
-}
-
-function setup_gsutils() {
-    echo "Setting up gsutils..."
-    sudo apt-get update -y
-    sudo apt-get install apt-transport-https ca-certificates gnupg curl -y
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
-    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-    sudo apt-get install google-cloud-cli -y
-
-    echo "$SERVICE_ACCOUNT_JSON" > ./zenpalio-f0ec2f137303.json
-
-    gcloud auth activate-service-account --key-file=./zenpalio-f0ec2f137303.json
-    gcloud config set project zenpalio
-    echo "finished setting up gsutils"
 }
 
 function pip_install() {
--- setup_automatic_debugging.sh
+++ setup_automatic_debugging.sh
@@ -1,4 +1,21 @@
 export BACKEND=automatic1111
+setup_gsutils
 wget -O - "/vastai-pyworker/start_server.sh" | bash && (text-generation-launcher --model-id "$MODEL_ID" --json-output --port 5001 --hostname "0.0.0.0" &>> $MODEL_LOG);
 /opt/ai-dock/bin/init.sh &
+
+
+function setup_gsutils() {
+    echo "Setting up gsutils..."
+    sudo apt-get update -y
+    sudo apt-get install apt-transport-https ca-certificates gnupg curl -y
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+    sudo apt-get install google-cloud-cli -y
+
+    echo "$SERVICE_ACCOUNT_JSON" > ./zenpalio-f0ec2f137303.json
+
+    gcloud auth activate-service-account --key-file=./zenpalio-f0ec2f137303.json
+    gcloud config set project zenpalio
+    echo "finished setting up gsutils"
+}
 
