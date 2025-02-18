fcd1a122c9feeb1883a689e87f31e9583b81b462
Fulll3 <kovarik.adam@email.cz>
2025-02-18 16:58:28 +0100

    wip

--- setup_automatic.sh
+++ setup_automatic.sh
@@ -1,7 +1,8 @@
 function setup_gsutils() {
     echo "Setting up gsutils..."
-    DEBIAN_FRONTEND=noninteractive sudo apt update -y
-    DEBIAN_FRONTEND=noninteractive sudo apt install apt-transport-https ca-certificates gnupg curl -y
+    export DEBIAN_FRONTEND=noninteractive
+    sudo apt-get update -y
+    sudo apt-get install apt-transport-https ca-certificates gnupg curl -y
 
     if [ -f /usr/share/keyrings/cloud.google.gpg ]; then
         sudo rm /usr/share/keyrings/cloud.google.gpg
@@ -9,7 +10,7 @@ function setup_gsutils() {
 
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
     echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-    DEBIAN_FRONTEND=noninteractive sudo apt install google-cloud-cli -y
+    sudo apt-get install google-cloud-cli -y
 
     echo "$SERVICE_ACCOUNT_JSON" > ./zenpalio-f0ec2f137303.json
 
@@ -20,7 +21,7 @@ function setup_gsutils() {
 
 setup_gsutils
 export BACKEND=automatic1111
-/opt/ai-dock/bin/init.sh &
+/opt/ai-dock/bin/init.sh 
 wget -O - "https://raw.githubusercontent.com/zenpalio/vastai-pyworker/main/start_server.sh" | bash && (text-generation-launcher --model-id "$MODEL_ID" --json-output --port 5001 --hostname "0.0.0.0" &>> $MODEL_LOG);
 
 
