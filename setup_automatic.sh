30a08987c8dc3b7c77c2fc7b91b32d508c18efe9
Fulll3 <kovarik.adam@email.cz>
2025-02-18 17:11:06 +0100

    wip

--- setup_automatic.sh
+++ setup_automatic.sh
@@ -22,7 +22,7 @@ function setup_gsutils() {
 
 setup_gsutils
 export BACKEND=automatic1111
-/opt/ai-dock/bin/init.sh 
+/opt/ai-dock/bin/init.sh  &
 wget -O - "https://raw.githubusercontent.com/zenpalio/vastai-pyworker/main/start_server.sh" | bash && (text-generation-launcher --model-id "$MODEL_ID" --json-output --port 5001 --hostname "0.0.0.0" &>> $MODEL_LOG);
 
 
