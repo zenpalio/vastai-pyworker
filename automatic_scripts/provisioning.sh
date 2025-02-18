7a02a74fde7273e00a97ec2af0800043aea064ec
Fulll3 <kovarik.adam@email.cz>
2025-02-18 18:20:40 +0100

    wip

--- automatic_scripts/provisioning.sh
+++ automatic_scripts/provisioning.sh
@@ -104,9 +104,10 @@ function setup_gsutils() {
     fi
 
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+    sleep 5s
     echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-    export DEBIAN_FRONTEND=noninteractive
-    DEBIAN_FRONTEND=noninteractive sudo apt-get install google-cloud-cli -y
+    sleep 5s
+    sudo apt-get install google-cloud-cli -y
 
     echo "$SERVICE_ACCOUNT_JSON" > ./zenpalio-f0ec2f137303.json
 
