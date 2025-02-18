c478b3abfdac45205894d3c8a47e08c659734e46
Fulll3 <kovarik.adam@email.cz>
2025-02-18 15:29:29 +0100

    wip

--- setup_automatic.sh
+++ setup_automatic.sh
@@ -60,4 +60,5 @@ python3 script.py
 
 export BACKEND=automatic1111
 wget -O - "https://raw.githubusercontent.com/zenpalio/vastai-pyworker/main/start_server.sh" | bash && (text-generation-launcher --model-id "$MODEL_ID" --json-output --port 5001 --hostname "0.0.0.0" &>> $MODEL_LOG);
+/opt/ai-dock/bin/init.sh &
 
