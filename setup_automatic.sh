d25e48c2bdf357a6ef5e85f5e0dee55b907779a8
Fulll3 <kovarik.adam@email.cz>
2025-03-21 11:54:38 +0100

    removing gsutils

--- setup_automatic.sh
+++ setup_automatic.sh
@@ -1,5 +1,4 @@
 export BACKEND=automatic1111
-wget -O - "https://raw.githubusercontent.com/zenpalio/vastai-pyworker/main/gsutils.sh" | bash && \
 (/opt/ai-dock/bin/init.sh &);
 wget -O - "https://raw.githubusercontent.com/zenpalio/vastai-pyworker/main/start_server.sh" | bash; (text-generation-launcher --model-id "$MODEL_ID" --json-output --port 5001 --hostname "0.0.0.0" &>> $MODEL_LOG);
 
