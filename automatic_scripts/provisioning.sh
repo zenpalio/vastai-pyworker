d706975169395fe4c646fdf2a5e9d3e50b95a662
Fulll3 <kovarik.adam@email.cz>
2025-03-27 17:57:40 +0100

    testing babes v3

--- automatic_scripts/provisioning.sh
+++ automatic_scripts/provisioning.sh
@@ -33,11 +33,13 @@ EXTENSIONS=(
 
 CHECKPOINT_MODELS=(
     #"https://civitai.com/api/download/models/929239?type=Model&format=SafeTensor&size=full&fp=fp16" #big lust15
-    "https://civitai.com/api/download/models/1094291?type=Model&format=SafeTensor&size=pruned&fp=fp16" #endgame
+    #"https://civitai.com/api/download/models/1094291?type=Model&format=SafeTensor&size=pruned&fp=fp16" #endgame
     #"https://civitai.com/api/download/models/926965?type=Model&format=SafeTensor&size=pruned&fp=fp16" #lustify40
-    "https://civitai.com/api/download/models/1047139?type=Model&format=SafeTensor&size=pruned&fp=fp16" #pony40
-    "https://civitai.com/api/download/models/373176?type=Model&format=SafeTensor&size=full&fp=fp16" #wildcardxXLPONY_wildcardxXLPONYV1
+    #"https://civitai.com/api/download/models/1047139?type=Model&format=SafeTensor&size=pruned&fp=fp16" #pony40
+    #"https://civitai.com/api/download/models/373176?type=Model&format=SafeTensor&size=full&fp=fp16" #wildcardxXLPONY_wildcardxXLPONYV1
     #"https://civitai.com/api/download/models/176425?type=Model&format=SafeTensor&size=pruned&fp=fp16" #test small model
+    "https://civitai.com/api/download/models/992946?type=Model&format=SafeTensor&size=full&fp=fp16"
+
 )
 
 LORA_MODELS=(
